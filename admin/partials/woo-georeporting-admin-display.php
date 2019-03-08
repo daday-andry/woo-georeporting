a9de7484d517aa0e74096500cc51d5c721f314cb
Andry Nirina <ani@esokia-webagency.com>
2019-03-08 11:17:19 +0300

    08-03-_Init_project

---
+++ admin/partials/woo-georeporting-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       andrynirina.portfoliobox.net
+ * @since      1.0.0
+ *
+ * @package    Woo_Georeporting
+ * @subpackage Woo_Georeporting/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-woo-georeporting-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       andrynirina.portfoliobox.net
+ * @since      1.0.0
+ *
+ * @package    Woo_Georeporting
+ * @subpackage Woo_Georeporting/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woo_Georeporting
+ * @subpackage Woo_Georeporting/includes
+ * @author     ANDRY Nirina <andrysahaedena@gmail.com>
+ */
+class Woo_Georeporting_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
